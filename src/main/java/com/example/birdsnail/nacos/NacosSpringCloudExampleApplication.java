ae84545ea529fc3b5c826ebb4c8307dff5344842
BirdSnail <314726028@qq.com>
2023-03-20 15:27:46 +0800

    feat: 使用spring config group demo

--- src/main/java/com/example/birdsnail/nacos/NacosSpringCloudExampleApplication.java
+++ src/main/java/com/example/birdsnail/nacos/NacosSpringCloudExampleApplication.java
@@ -3,19 +3,26 @@ package com.example.birdsnail.nacos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class NacosSpringCloudExampleApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(NacosSpringCloudExampleApplication.class, args);
-        String userName = applicationContext.getEnvironment().getProperty("person.name");
-        String userAge = applicationContext.getEnvironment().getProperty("person.age");
-        String dog = applicationContext.getEnvironment().getProperty("dog.name");
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        String userName = environment.getProperty("person.name");
+        String userAge = environment.getProperty("person.age");
+        String dog = environment.getProperty("dog.name");
         System.out.println("dog:" + dog);
-        String cat = applicationContext.getEnvironment().getProperty("cat.name");
+        String cat = environment.getProperty("cat.name");
         System.out.println("cat:" + cat);
         System.err.println("user name :" + userName + "; age: " + userAge);
+
+        System.out.println("=========spring config group分组测试=============");
+        System.out.printf("group1-->{name:%s, desc:%s}%n", environment.getProperty("test.group1.name"), environment.getProperty("test.group1.desc"));
+        System.out.printf("group2-->{name:%s, desc:%s}%n", environment.getProperty("test.group2.name"), environment.getProperty("test.group2.desc"));
+
     }
 
 
